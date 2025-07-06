61c1523bd4afb4d95689204c531941e55fe39865
syahruncega <syahruncega@gmail.com>
2025-07-06 23:51:05 +0800

    Navbar Responsive

--- src/app/components/home/hero/content.tsx
+++ src/app/components/home/hero/content.tsx
@@ -26,22 +26,7 @@ const HeroContent: FC<{ data: any }> = ({ data }) => {
       <div className="container relative z-10">
         <div ref={ref} className="flex flex-col gap-8">
           {/* ---------------- heading text --------------- */}
-          <motion.div
-            {...bottomAnimation}
-            className="relative flex flex-col text-center items-center gap-4"
-          >
-            {/* <h1 className="font-medium w-full">
-                Building bold brands with
-                <span className="instrument-font italic font-normal dark:text-white/70">
-                  {" "}
-                  thoughtful design
-                </span>
-              </h1>
-              <p className="max-w-38 text-dark_black/60 dark:text-white/60">
-                At Awake, we help small startups tackle the world’s biggest
-                challenges with tailored solutions, guiding you from strategy to
-                success in a competitive market.
-              </p> */}
+          <motion.div {...bottomAnimation}>
             <Carousel
               opts={{ loop: true }}
               plugins={[
@@ -67,82 +52,11 @@ const HeroContent: FC<{ data: any }> = ({ data }) => {
                   </CarouselItem>
                 ))}
               </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
-            </Carousel>{" "}
+
+              <CarouselPrevious className="hidden lg:block lg:pl-2" />
+              <CarouselNext className="hidden lg:block lg:pl-2" />
+            </Carousel>
           </motion.div>
-
-          {/* <motion.div
-              {...bottomAnimation}
-              className="flex flex-col items-center justify-center gap-4"
-            >
-              <div className="flex flex-col items-center justify-center gap-8 w-full sm:flex-row">
-                <Link
-                  href="/contact"
-                  className="group bg-purple_blue text-white font-medium flex flex-row justify-between items-center py-2 px-5 rounded-full max-w-64 w-full md:py-3 border border-purple_blue transition-all duration-200 ease-in-out hover:bg-transparent hover:text-purple_blue"
-                >
-                  <span className="flex text-start transform transition-transform duration-200 ease-in-out group-hover:translate-x-28">
-                    Get Started
-                  </span>
-                  <svg
-                    width="40"
-                    height="40"
-                    viewBox="0 0 40 40"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="transform transition-transform duration-200 ease-in-out group-hover:-translate-x-44 group-hover:rotate-45"
-                  >
-                    <rect
-                      width="40"
-                      height="40"
-                      rx="20"
-                      className="fill-white transition-colors duration-200 ease-in-out group-hover:fill-purple_blue"
-                    />
-                    <path
-                      d="M15.832 15.3334H24.1654V23.6667"
-                      className="stroke-[#1B1D1E] transition-colors duration-200 ease-in-out group-hover:stroke-white"
-                      strokeWidth="1.66667"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <path
-                      d="M15.832 23.6667L24.1654 15.3334"
-                      className="stroke-[#1B1D1E] transition-colors duration-500 ease-in-out group-hover:stroke-white"
-                      strokeWidth="1.66667"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </Link>
-
-                <div className="flex items-center gap-7">
-                  <ul className="avatar flex flex-row items-center">
-                    {avatarList?.avatarList?.map((items: any, index: any) => (
-                      <li key={index} className="-mr-2 z-1 avatar-hover:ml-2">
-                        <Image
-                          src={items.image}
-                          alt="Image"
-                          width={44}
-                          height={44}
-                          quality={100}
-                          className="rounded-full border-2 border-white"
-                          unoptimized={true}
-                        />
-                      </li>
-                    ))}
-                  </ul>
-
-                  <div className="gap-1 flex flex-col">
-                    <div>
-                      <StarRating count={4} color="#F59E0B" />
-                    </div>
-                    <p className="text-sm font-normal text-dark_black/60 dark:text-white/60">
-                      Trusted by 1000+ clients
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </motion.div> */}
         </div>
       </div>
     </div>
