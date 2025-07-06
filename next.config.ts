f707db3696a0b21036f2d4fae16c9c4638241fb3
syahruncega <syahruncega@gmail.com>
2025-07-07 05:59:03 +0800

    Adding config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.amazonaws.com",
+      },
+    ],
   },
 };
 
