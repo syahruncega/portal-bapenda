af40dc062a3dd10287e98b026ec0264b0db2c4b7
syahruncega <syahruncega@gmail.com>
2025-09-20 19:04:08 +0800

    Ready to ship

--- src/app/(site)/p/(ppid)/daftar-informasi/page.tsx
+++ src/app/(site)/p/(ppid)/daftar-informasi/page.tsx
@@ -4,6 +4,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/app/components/ui/accordion";
+import { Dot } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
 import React from "react";
@@ -12,6 +13,39 @@ export const metadata: Metadata = {
   title: "Daftar Informasi - PPID | BAPENDA",
 };
 
+const items = [
+  {
+    id: "1",
+    title: "Rencana Strategis (RENSTRA)",
+    contents: [
+      {
+        label: "Renstra 2024-2026",
+        href: `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}/uploads/RENSTRA_BAPENDA_2024_2026_cf4d2b600c.pdf`,
+      },
+    ],
+  },
+  {
+    id: "2",
+    title: "Rencana Kerja (RENJA)",
+    contents: [
+      {
+        label: "Renja 2025",
+        href: `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}/uploads/RENJA_2025_BAPENDA_d60e3e5f04.pdf`,
+      },
+    ],
+  },
+  {
+    id: "3",
+    title: "LAKIP",
+    contents: [
+      {
+        label: "Lakip 2024",
+        href: `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}/uploads/LAKIP_BAPENDA_2024_3fc13ed528.pdf`,
+      },
+    ],
+  },
+];
+
 const Page = () => {
   return (
     <article className="mt-40">
@@ -19,35 +53,34 @@ const Page = () => {
         <h2 className="text">Daftar Informasi</h2>
       </div>
 
-      <div className="mt-14 flex justify-center mx-20">
+      <div className="mt-14 flex justify-center mx-10">
         <Accordion
           type="single"
           collapsible
-          className="w-3xl"
-          defaultValue="item-1"
+          className="space-y-2 lg:w-3xl"
+          defaultValue="3"
         >
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Rencana Strategis (RENSTRA)</AccordionTrigger>
-            <AccordionContent className="flex flex-col gap-4 text-balance">
-              <Link
-                href={`${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}/uploads/RENSTRA_BAPENDA_2024_2026_cf4d2b600c.pdf`}
-                target="_blank"
-              >
-                Renstra 2024-2026
-              </Link>
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger>Rencana Kerja (RENJA)</AccordionTrigger>
-            <AccordionContent className="flex flex-col gap-4 text-balance">
-              <Link
-                href={`${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}/uploads/RENJA_2025_BAPENDA_d60e3e5f04.pdf`}
-                target="_blank"
-              >
-                Renja 2025
-              </Link>
-            </AccordionContent>
-          </AccordionItem>
+          {items.map((item) => (
+            <AccordionItem
+              value={item.id}
+              key={item.id}
+              className="bg-background mt-2 has-focus-visible:border-ring has-focus-visible:ring-ring/50 rounded-md border px-4 py-1 outline-none last:border-b has-focus-visible:ring-[3px]"
+            >
+              <AccordionTrigger className="py-2 text-[15px] leading-6 hover:no-underline focus-visible:ring-0">
+                {item.title}
+              </AccordionTrigger>
+              <AccordionContent className="text-muted-foreground pb-2">
+                {item.contents.map((content) => (
+                  <div key={content.label} className="flex space-y-2">
+                    <Dot />
+                    <Link href={content.href} className="" target="_blank">
+                      {content.label}
+                    </Link>
+                  </div>
+                ))}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
         </Accordion>
       </div>
     </article>
--- src/app/components/ui/accordion.tsx
+++ src/app/components/ui/accordion.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import * as React from "react"
-import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDownIcon } from "lucide-react"
+import { Accordion as AccordionPrimitive } from "radix-ui"
 
 import { cn } from "@/lib/utils"
 
@@ -35,13 +35,17 @@ function AccordionTrigger({
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
+          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-center justify-between gap-4 rounded-md py-4 text-left text-sm font-semibold transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
           className
         )}
         {...props}
       >
         {children}
-        <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
+        <ChevronDownIcon
+          size={16}
+          className="pointer-events-none shrink-0 opacity-60 transition-transform duration-200"
+          aria-hidden="true"
+        />
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
   )
@@ -63,4 +67,4 @@ function AccordionContent({
   )
 }
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export { Accordion, AccordionContent, AccordionItem, AccordionTrigger }
